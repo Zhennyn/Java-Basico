1f650e849ea816248df4953bbcb5404c5d4f09b4
Zhennyn <matheuslimamenezes2005@gmail.com>
2024-05-20 04:04:47 -0300

    Operações

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,18 +1,47 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
 
-        String concatenacao ="?";
+        String concatenacao = "?";
 
-        concatenacao = 1+1+1+"1";
-        concatenacao = 1+"1"+1+"1";
-        concatenacao = "1"+1+1+1;
-        concatenacao = 1+1+1+"1";
-        concatenacao = (1+1+1)+"1";
+        concatenacao = 1 + 1 + 1 + "1";
+        concatenacao = 1 + "1" + 1 + "1";
+        concatenacao = "1" + 1 + 1 + 1;
+        concatenacao = 1 + 1 + 1 + "1";
+        concatenacao = (1 + 1 + 1) + "1";
 
         System.out.println(concatenacao);
 
-
         String nomeCompleto = "LINGUAGEM " + "JAVA";
         System.out.println(nomeCompleto);
+
+        // x repetição, incrementa +1 no ++ e decremento -1 no --
+        int numero = 5;
+
+        numero++;
+        System.out.println(numero);
+
+        int a, b;
+        String resultado = "";
+        a = 5;
+        b = 5;
+
+        if (a == b)
+            resultado = "verdadeiro ";
+        else
+            resultado = "falso ";
+
+        System.out.print(resultado);
+
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das duas condições são verdadeiras");
+        }
     }
 }
