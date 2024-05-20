c149519c38401a952e9008021330ad029374684b
Zhennyn <matheuslimamenezes2005@gmail.com>
2024-05-20 03:36:52 -0300

    Operadores

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,5 +1,18 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+        concatenacao = 1+"1"+1+"1";
+        concatenacao = "1"+1+1+1;
+        concatenacao = 1+1+1+"1";
+        concatenacao = (1+1+1)+"1";
+
+        System.out.println(concatenacao);
+
+
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        System.out.println(nomeCompleto);
     }
 }
