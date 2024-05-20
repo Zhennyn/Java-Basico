c5bb9359e403eaac6d258fd63e53919ea6486e31
Zhennyn <matheuslimamenezes2005@gmail.com>
2024-05-20 05:05:37 -0300

    Palavras Reservadas

---
+++ PalavrasReservadas.java
@@ -0,0 +1,52 @@
+//*Controle de pacotes */
+//import
+//package
+
+//*Modificadores de acesso */
+//public
+//private
+//protected
+
+
+//*Primitivos */
+//boolean
+//char
+//double
+//byte
+//float
+//long
+//short
+//void
+//int
+
+//*Modificadores de classes */
+//abstract
+//class
+//extends
+//final
+//implements
+//interface
+//native
+//new
+//static
+//strictfp
+//synchronized
+//transient
+//volatile
+
+//*Controle de fluxo dentro de um bloco de código */
+//break
+//case
+//continue
+
+//*Tratamento de erros */
+//assert
+//catch
+//finally
+//throw
+//throws
+//try
+
+//*Variáveis de referência */
+//super
+//this
